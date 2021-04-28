bfb4cc0b959e78e73c9caca3b1b2ac106694a7bb
Kay Kim <kay.kim@inrupt.com>
2021-04-27 21:53:12 -0400

    DOCS-296: Use shared docs-assets

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ extlinks = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['./_templates']
+templates_path = ['./docs-assets/_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -80,7 +80,7 @@ exclude_patterns = ['includes']
 #html_theme = 'alabaster'
 
 html_theme = 'inrupt'
-html_theme_path = ['./themes']
+html_theme_path = ['./docs-assets/themes']
 
 html_copy_source = False
 
@@ -95,12 +95,11 @@ html_theme_options = {
     'banner_msg': '',
     'robots_index': True,
     'github_editable': False,
+    'docs_project': 'developer-tools/javascript/react-sdk',
     # 'github_org': 'inrupt',
     # 'github_repo': 'docs-client-lib-js',
     # 'github_branch': 'master',
-    'ess_docs': 'https://docs.inrupt.com/ess/',
-    'clientlibjs_docs': 'https://docs.inrupt.com/developer-tools/javascript/client-libraries',
-    'reactsdk_docs': 'https://docs.inrupt.com/developer-tools/javascript/react-sdk',
+
     
     # below are pydata_sphinx_theme
     "footer_items": [ "copyright.html"],
@@ -134,7 +133,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['./docs-assets/_static']
 
 html_sidebars = {
     '**': [ 'search-field.html',  'docs-sidebar.html'],
